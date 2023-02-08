1f063a348138e4c93e70d0dba3848905c0792dea
seyi103 <74170226+seyi103@users.noreply.github.com>
2023-02-08 11:13:40 +0900

    Update 손세이.cpp

--- week1/SWEA/수영장/손세이.cpp
+++ week1/SWEA/수영장/손세이.cpp
@@ -1,1 +1,45 @@
 
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int MIN;
+int plan[13];
+int price[4]; 
+
+void dfs(int month, int cost) {
+	if (month >= 12) {
+		if (cost < MIN) MIN = cost;
+		return;
+	}
+	if (plan[month + 1] == 0)
+		dfs(month + 1, cost);
+	else {
+		dfs(month + 1, cost + price[0] * plan[month + 1]);
+		dfs(month + 1, cost + price[1]);
+		dfs(month + 3, cost + price[2]);
+	}
+}
+
+int main() {
+	int T;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		//reset
+		memset(plan, 0, sizeof(plan));
+		memset(price, 0, sizeof(price));
+		MIN = 21e8;
+		//input
+		for (int i = 0; i < 4; i++)
+			cin >> price[i];
+		for (int i = 1; i <= 12; i++)
+			cin >> plan[i];
+		//solve
+		if (MIN > price[3]) 
+			MIN = price[3];
+		dfs(0, 0);
+		//output
+		cout << "#" << tc << " " << MIN << '\n';
+	}
+	return 0;
+}
