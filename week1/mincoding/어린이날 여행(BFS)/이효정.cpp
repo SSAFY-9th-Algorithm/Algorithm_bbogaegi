1e9f84bbe137d348f9e804c5a56fceb960fedbc1
201611099 <hyojlee@student.42seoul.kr>
2023-02-11 12:17:10 +0900

    Add: BFS DFS로 접근 시 런타임에러 발생 visited와 비슷한 역할의 used를 만들었는데 둘을 하나로 줄여줌

--- week1/mincoding/어린이날 여행(BFS)/이효정.cpp
+++ week1/mincoding/어린이날 여행(BFS)/이효정.cpp
@@ -1,1 +1,46 @@
-test
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+// 도시 개수, 도로 개수, 남은 기름, 출발도시
+int N, M, K, P;
+vector<int> MAP[200001];
+int visited[200001];
+
+void bfs(int start) {
+	queue<int> q;
+	q.push(start);
+	visited[start] = 1;
+	while (!q.empty()) {
+		int now = q.front();
+		q.pop();
+		for (int i = 0; i < MAP[now].size(); i++) {
+			int next = MAP[now][i];
+			if (visited[next])
+				continue;
+			visited[next] = visited[now] + 1;
+			q.push(next);
+		}
+	}
+}
+
+int main(void) {
+	cin >> N >> M >> K >> P;
+	for (int i = 0; i < M; i++) {
+		int from, to;
+		cin >> from >> to;
+		MAP[from].push_back(to); // 단방향 연결
+	}
+	bfs(P);
+	int flag = 0;
+	for (int i = 1; i <= N; i++) {
+		if (visited[i] - 1 == K) {
+			flag = 1;
+			cout << i << '\n';
+		}
+	}
+	if (!flag) // 기름통을 모두 소모하여 살 수 있는 도시가 없음
+		cout << "-1\n";
+	return 0;
+}
