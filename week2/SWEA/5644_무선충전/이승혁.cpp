1319fdf6f39faefbd5206ececeeff2a97a13fb8f
leeseunghyuk0228 <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-02-18 21:59:43 +0900

    Update 이승혁.cpp

--- week2/SWEA/5644_무선충전/이승혁.cpp
+++ week2/SWEA/5644_무선충전/이승혁.cpp
@@ -1,29 +1,121 @@
-// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###
-// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###
-// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###
-// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###
-// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###
-// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###// ### 미해결 문제 ###
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+struct Charger {
+    int x,y;
+    int range, power;
+};
+
+int Move[2][100];
+int da[5][2] = { {0,0},{-1,0},{0,1},{1,0},{0,-1} };
+int M, C, X1, X2, Y1, Y2 , sum;
+
+Charger charger[8];
+vector<int> v[2];
+
+bool cmp(Charger left, Charger right) {
+    if (left.power < right.power)
+        return false;
+    if (left.power > right.power)
+        return true;
+    return false;
+}
+
+void moving(int time) {
+    int ch1d = Move[0][time];
+    Y1 = Y1 + da[ch1d][0];
+    X1 = X1 + da[ch1d][1];
+
+    int ch2d = Move[1][time];
+    Y2 = Y2 + da[ch2d][0];
+    X2 = X2 + da[ch2d][1];
+}
+
+void findCharger() {
+    for (int i = 0; i < C; i++) {
+        if ((abs(charger[i].y -Y1) + abs(charger[i].x - X1)) <= charger[i].range) 
+            v[0].push_back(i);
+        if ((abs(charger[i].y - Y2) + abs(charger[i].x - X2)) <= charger[i].range)
+            v[1].push_back(i);
+    }
+}
+
+void charging() {
+
+    findCharger();
+
+    int v1 = v[0].size();
+    int v2 = v[1].size();
+
+    if (v1 == 0 && v2 == 0) return;
+
+    if (v1 == 0) {
+        sum += charger[v[1][0]].power;
+        return;
+    }
+    else if (v2 == 0) {
+        sum += charger[v[0][0]].power;
+        return;
+    }
+
+    if (v[0][0] == v[1][0])
+    {
+        int temp = 0;
+        for (int i = 0; i < v1; i++) {
+            for (int j = 0; j < v2; j++) {
+                if (charger[v[0][i]].power == charger[v[1][j]].power) temp = max(temp, charger[v[0][i]].power);
+                else temp = max(charger[v[0][i]].power + charger[v[1][j]].power, temp);
+            }
+        }
+        sum += temp;
+        return;
+    }
+ 
+    else {
+        sum += charger[v[0][0]].power;
+        sum += charger[v[1][0]].power;
+    }
+
+}
 
 
-// 각 충전기 위치에서 flood fill 
-// 맵에 충전기의 번호를 나타낸다
+int main()
+{
+    int T;
+    cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        // reset
+        X1 = 1;
+        Y1 = 1;
+        X2 = 10;
+        Y2 = 10;
+        sum = 0;
+        memset(charger, 0, sizeof(charger));
+        memset(Move, 0, sizeof(Move));
+        // input
+        cin >> M >> C;
+        for (int i = 0; i < 2; i++) for (int j = 0; j < M; j++) cin >> Move[i][j];
+        for (int i = 0; i < C; i++) {
+            Charger temp;
+            cin >> temp.x >> temp.y >> temp.range >> temp.power;
+            charger[i] = temp;
+        }
+        sort(charger, charger + C, cmp);
 
-0 2 0 1 0 0 0 0
-2 2 1/2 1 1 0 0 0
-2 2 2 1/2 0 0 0 0
-2 2 2 1 0 0 0 0
-0 2 0 0 3 0 0 0
-0 0 0 3 3 3 0 0
-0 0 3 3 3 3 3 0
-  
-// 범위가 겹치는 녀석들을 확인할 수 없다.
-// move 방향대로 각 캐릭터를 이동하며 if map[ny][nx]!=0 , sum1+=BC[map[ny1][nx1]] , sum2=BC[map[ny2][nx2]] 를 구현하고
-// if ny1==ny2 && nx2==ny22 둘이 같이있다면, 어떤게 더 효율적일까 확인
-// 각 인원이 이용 가능한 개수 비교(어떻게?
-// 개수가같을때? 둘다 한개? 둘ㄷ 다 두개?-> A가 1번, B가 2번, B가 1번 A가 1번 
-// 한 명 한 개, 한 명 두 개 .. 아 이렇게 하는게 맞나 이렇게 조건이 많아지면 점점 잘못 설계하고 있다는 생각이
-// 위에서 시작을 잘못했기에 점차 해야할 게 많아지는 느낌?
-  
-  // 눈이 슬슬 감기고.. 정리할게 많아서 주말에 풀어보는 걸로..
-  // 해결 방안은 내일 조금 더 생각해보고 조언을 구하던
+        // solve
+        for (int time = 0; time <= M; time++) {
+            charging();
+            v[0].clear();
+            v[1].clear();
+            moving(time);
+        }
+
+        // output
+        cout <<"#"<<tc<<" "<<sum<<'\n';
+    }
+
+
+}
