97e5dc5b31cee4a5c15b259b34b0e552df50fb34
leeseunghyuk0228 <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-02-21 15:25:55 +0900

    Update 이승혁.cpp

--- week3/day1_minco_split_region/이승혁.cpp
+++ week3/day1_minco_split_region/이승혁.cpp
@@ -1,93 +1,97 @@
-#################미해결 문제########################
-#################미해결 문제########################
-#################미해결 문제########################
-#################미해결 문제########################
-#################미해결 문제########################
-#################미해결 문제########################
-
 #include <iostream>
 #include <vector>
 #include <cstring>
 using namespace std;
 
-int MAP[8][8],w[8];
-int N,res,choice[8];
-int plused[8];
+int MAP[8][8], w[8];
+int N, res, choice[8];
 
 void isValid() {
-	int A=0, B=0;
+	int A = 0, B = 0, Aflag = 0, Bflag = 0;
 	vector<int> Av;
 	vector<int> Bv;
-
-	memset(plused, 0, sizeof(plused));
 
 	for (int i = 0; i < N; i++) {
 		if (choice[i] == 1) Av.push_back(i);
 		else Bv.push_back(i);
 	}
-	if (Av.size() == 0 || Bv.size() == 0) return;
-	// A 마을 확인
-	if (Av.size() == 1) A += w[Av[0]];
-	else if(Av.size()>1)
-	{
-		for (int i = 0; i < Av.size() - 1; i++) {
-			for (int j = i + 1; j < Av.size(); j++) {
-				// 같은 지역구로 선택된 두 마을이 연결될 수 있다면
-				//cout << Av[i] << " " << Av[j] << '\n';
-				if (MAP[Av[i]][Av[j]] && MAP[Av[j]][Av[i]]) {
-					if (plused[Av[i]] == 0) {
-						plused[Av[i]] = 1;
-						A += w[Av[i]];
-					}
-					if (plused[Av[j]] == 0) {
-						plused[Av[j]] = 1;
-						A += w[Av[j]];
-					}
-				}
 
+	if (Av.size() == 0 || Bv.size() == 0)return;
+
+	// A 마을 확인
+	if (Av.size() == 1) Aflag = 1;
+	else if (Av.size() > 1)
+	{
+		// 간선의 개수는 최소 N-1 개 이상
+		int Acnt = 0;
+		for (int i = 0; i < Av.size(); i++) {
+			// 하나도 이어지지 않았다면 그대로 0
+			int Asolo = 0;
+			for (int j = 0; j < Av.size(); j++) {
+				if (i == j) continue;
+				if (MAP[Av[i]][Av[j]] == 1) {
+					Acnt++;
+					Asolo = 1;
+				}
 			}
+			if (Asolo == 0) return;
 		}
+		if (Acnt / 2 >= Av.size() - 1) Aflag = 1;
 	}
 
 	// B 마을 확인
-	if (Bv.size() == 1) B += w[Bv[0]];
-	else if (Bv.size()>1)
+	if (Bv.size() == 1) Bflag = 1;
+	else if (Bv.size() > 1)
 	{
-		for (int i = 0; i < Bv.size() - 1; i++) {
-			for (int j = i + 1; j < Bv.size(); j++) {
-				// 같은 지역구로 선택된 두 마을이 연결될 수 있다면
-				if (MAP[Bv[i]][Bv[j]] && MAP[Bv[j]][Bv[i]]) {
-					if (plused[Bv[i]] == 0) {
-						plused[Bv[i]] = 1;
-						B += w[Bv[i]];
-					}
-					if (plused[Bv[j]] == 0) {
-						plused[Bv[j]] = 1;
-						B += w[Bv[j]];
-					}
+		int Bcnt = 0;
+		for (int i = 0; i < Bv.size(); i++) {
+			int Bsolo = 0;
+			for (int j = 0; j < Bv.size(); j++) {
+				if (i == j) continue;
+				if (MAP[Bv[i]][Bv[j]] == 1) {
+					Bsolo = 1;
+					Bcnt++;
 				}
-
 			}
+			if (Bsolo == 0) return;
+			if (Bcnt / 2 >= Bv.size() - 1) Bflag = 1;
 		}
 	}
+	// 유권수 추가 확인 초기화
 
+
+	if (Aflag == 1 && Bflag == 1) {
+		for (int i = 0; i < Av.size(); i++) A += w[Av[i]];
+		for (int i = 0; i < Bv.size(); i++) B += w[Bv[i]];
+		res = min(res, abs(A - B));
 	
+		//cout << "A 지역구 뽑힌 마을 : ";
+		//for (int i = 0; i < Av.size(); i++) {
+		//	cout << Av[i] << " ";
+		//}cout << '\n';
+		//cout << "B 지역구 뽑힌 마을 : ";
+		//for (int i = 0; i < Bv.size(); i++) {
+		//	cout << Bv[i] << " ";
+		//}cout << '\n';
+	}
 	
-	cout << "A 마을 유권자 수 " << A << " B 마을 유권자 수 " << B << "\n";
-	res = min(res, abs(A - B));
+
 }
 
 void dfs(int level) {
-	if (level == N/2) {
+	if (level == N / 2) {
 		isValid();
 		return;
 	}
 
 	for (int i = 0; i < N; i++) {
 		if (choice[i] == 1) continue;
+		// i 마을을 선택한다.
 		choice[i] = 1;
 		dfs(level + 1);
 		choice[i] = 0;
+
+		// i 마을을 선택하지 않는다.
 		dfs(level + 1);
 	}
 }
@@ -110,10 +114,11 @@ int main() {
 		// solve
 		dfs(0);
 		// output
-		cout <<"#"<<tc<<" "<<res<<'\n';
+		if (res == 21e8)
+			cout << "#" << tc << " " << 0 << '\n';
+		else
+			cout << "#" << tc << " " << res << '\n';
 	}
-	
+
 
 }
-
-
