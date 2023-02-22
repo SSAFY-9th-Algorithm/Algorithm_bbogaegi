c1a948a9cc69b350c0676c9816624ab946296810
hyojlee <hyojlee@student.42seoul.kr>
2023-02-22 11:48:29 +0900

    Done: Dijkstra

--- week3/day2_minco_mountain_robot/이효정.cpp
+++ week3/day2_minco_mountain_robot/이효정.cpp
@@ -7,9 +7,14 @@ using namespace std;
 /*
 dijkstra를 이용할 것
 - 4방향 + 터널
-터널을 반복문으로 확인할 때 시간초과 발생
--> 터널에 대한 처리를 다시해줘야할 것 같다.
--> 부분부분 오답도 
+
+문제였던 부분: 동일한 값들도 pq에 넣어줘서 오버헤드가 너무 커지는 문제
+	=> 시간초과 해결, next의 dist를 갱신시키는 경우에는 무조건 등호를 포함하여 continue 체크를 해줘야한다.
+
+dist는 최소값으로 갱신이 되어야하기 때문에 dist가 초기값이 아니라면 continue하는 부분은 없어야한다!
+visited != dist
+
+feat. 득화 교수님 & 내 깐부 
 */
 
 struct Node {
@@ -22,10 +27,16 @@ struct Node {
 	}
 };
 
+struct tNode {
+	int ay, ax;
+	int by, bx;
+	int cost;
+};
+
 int N, M; // 지도 한 변의 길이, 터널 개수
-int mat[40][40];
-int dist[40][40];
-vector<Node> tunnel[40][40];
+int mat[32][32];
+int dist[32][32];
+vector<tNode> tunnel;
 
 int ydir[] = { -1, 1, 0, 0 };
 int xdir[] = { 0, 0, -1, 1 };
@@ -34,11 +45,7 @@ int xdir[] = { 0, 0, -1, 1 };
 void reset() {
 	memset(mat, 0, sizeof(mat));
 	memset(dist, 0, sizeof(dist));
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			tunnel[i][j].clear();
-		}
-	}
+	tunnel.clear();
 }
 
 void input() {
@@ -52,26 +59,40 @@ void input() {
 	for (int i = 0; i < M; i++) {
 		int ay, ax, by, bx, c;
 		cin >> ay >> ax >> by >> bx >> c;
-		tunnel[ay][ax].push_back({ by, bx, c });
-		tunnel[by][bx].push_back({ ay, ax, c });
+		tunnel.push_back({ ay, ax, by, bx, c });
 	}
 }
 
 void dijkstra(int sy, int sx) {
 	priority_queue<Node> pq;
-	dist[sy][sx] = 1;
-	pq.push({ sy, sx, 1 });
+	dist[sy][sx] = 0;
+	pq.push({ sy, sx, 0 });
 	while (!pq.empty()) {
 		Node now = pq.top();
 		pq.pop();
 		if (now.cost > dist[now.y][now.x])
 			continue;
+		for (int i = 0; i < tunnel.size(); i++) {
+			tNode tNow = tunnel[i];
+			if (now.y == tNow.ay && now.x == tNow.ax) {
+				int tCost = tNow.cost + dist[now.y][now.x];
+				if (tCost >= dist[tNow.by][tNow.bx])
+					continue;
+				dist[tNow.by][tNow.bx] = tCost;
+				pq.push({ tNow.by, tNow.bx, tCost });
+			}
+			else if (now.y == tNow.by && now.x == tNow.bx) {
+				int tCost = tNow.cost + dist[now.y][now.x];
+				if (tCost >= dist[tNow.ay][tNow.ax])
+					continue;
+				dist[tNow.ay][tNow.ax] = tCost;
+				pq.push({ tNow.ay, tNow.ax, tCost });
+			}
+		}
 		for (int i = 0; i < 4; i++) {
 			int ny = now.y + ydir[i];
 			int nx = now.x + xdir[i];
 			if (ny < 1 || nx < 1 || ny > N || nx > N)
-				continue;
-			if (dist[ny][nx] != 21e8)
 				continue;
 			int diff = mat[ny][nx] - mat[now.y][now.x];
 			if (diff < 0) {
@@ -83,17 +104,10 @@ void dijkstra(int sy, int sx) {
 				diff *= 2;
 			}
 			diff += dist[now.y][now.x];
-			if (dist[ny][nx] < diff)
+			if (dist[ny][nx] <= diff)
 				continue;
 			dist[ny][nx] = diff;
 			pq.push({ ny, nx, dist[ny][nx] });
-		}
-		for (int i = 0; i < tunnel[now.y][now.x].size(); i++) {
-			Node next = tunnel[now.y][now.x][i];
-			if (next.cost + dist[now.y][now.x] > dist[next.y][next.x])
-				continue;
-			dist[next.y][next.x] = next.cost + dist[now.y][now.x];
-			pq.push({ next.y, next.x, dist[next.y][next.x] });
 		}
 	}
 }
@@ -109,6 +123,6 @@ int main(void) {
 		// solve
 		dijkstra(1, 1);
 		// output
-		cout << "#" << tc << " " << dist[N][N] - 1 << '\n';
+		cout << "#" << tc << " " << dist[N][N] << '\n';
 	}
 }
