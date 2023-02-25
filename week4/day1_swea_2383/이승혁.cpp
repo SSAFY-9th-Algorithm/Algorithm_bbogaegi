b9edb97836fb27883cc351dd599d1faa955de1c0
leeseunghyuk0228 <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-02-25 18:05:07 +0900

    testcast49/50

--- week4/day1_swea_2383/이승혁.cpp
+++ week4/day1_swea_2383/이승혁.cpp
@@ -1,3 +1,6 @@
+// 가장 적게걸리는 -> 모든 경우 + 최소값
+// ===> 가지치기
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <queue>
@@ -13,14 +16,12 @@ struct person {
 	int num;
 };
 
-struct cmps1 {
-	bool operator () (person left, person right) {
+bool cmps1(person left, person right) {
 		if (left.s1t < right.s1t)
-			return false;
-		if (left.s1t > right.s1t)
 			return true;
+		if (left.s1t > right.s1t)
+			return false;
 		return false;
-	}
 };
 struct cmps2 {
 	bool operator () (person left, person right) {
@@ -34,27 +35,23 @@ struct cmps2 {
 
 coord stair[2];
 person people[10];
+vector<int>path;
 
-int N, p_cnt, s_cnt, MAP[10][10], finish[10];
+int N, p_cnt, s_cnt, MAP[10][10],MINTIME;
 int idx1, idx2;
 int canEnter1, canEnter2;
-priority_queue<person,vector<person>,cmps1>pq1;
-priority_queue<person,vector<person>,cmps2>pq2;
-vector<int>st1time;
-vector<int>st2time;
 
 void reset() {
 	idx1 = 0;
 	idx2 = 0;
+	MINTIME = 21e8;
 	canEnter1 = -21e8;
 	canEnter2 = -21e8;
 	p_cnt = 0;
 	s_cnt = 0;
 	memset(MAP, 0, sizeof(MAP));
 	memset(stair, 0, sizeof(stair));
-	memset(finish, 0, sizeof(finish));
-	st1time.clear();
-	st2time.clear();
+	path.clear();
 }
 
 void input() {
@@ -78,140 +75,112 @@ void calctime() {
 		p.s1t = abs(p.y - stair[0].y) + abs(p.x - stair[0].x) + stair[0].t ;
 		p.s2t = abs(p.y - stair[1].y) + abs(p.x - stair[1].x) + stair[1].t;
 		people[i] = p;
-		pq1.push({ people[i] });
-		pq2.push({ people[i] });
 	}
 }
 
-void steps(int v) {
-	if (v == 0) {
-		// 0 번계단이 가까운 사람들
-		int cursize = pq1.size();
-		for (int i = 0; i < cursize; i++) {
-			person now = pq1.top();
-			if (finish[now.num] == 1) {
-				pq1.pop();
-				continue;
-			}
-			finish[now.num] = 1;
-			if (idx1 < 3) {
-				canEnter1 = max(canEnter1, now.s1t);
-				st1time.push_back(now.num);
-			}
-			else if (idx1 >= 3 && now.s1t - people[st1time[idx1 - 3]].s1t >= stair[0].t) {
-				canEnter1 = max(canEnter1, now.s1t);
-			}
-			// 3번째 앞 사람이 끝나야 이동 가능
-			else {
-				canEnter1 = people[st1time[idx1 - 3]].s1t + stair[0].t;
-			}
-			idx1++;
-			pq1.pop();
-			break;
-		}
-	}
-	else {
-		// 1번 계단이 가까운 사람들
-		int cursize2 = pq2.size();
-		for (int i = 0; i < cursize2; i++) {
-			person now = pq2.top();
-			if (finish[now.num] == 1) {
-				pq2.pop();
-				continue;
-			}
-			finish[now.num] = 1;
-			if (idx2 < 3) {
-				canEnter2 = max(canEnter2, now.s2t);
-				st2time.push_back(now.num);
-			}
-			else if (idx2 >= 3 && now.s2t - people[st2time[idx2 - 3]].s2t >= stair[1].t) {
-				canEnter2 = max(canEnter2, now.s2t);
-			}
-			// 3번째 앞 사람이 끝나야 이동 가능
-			else {
-				canEnter2 = people[st2time[idx2 - 3]].s2t + stair[2].t;
-			}
-			idx2++;
-			pq2.pop();
-			break;
-		}
-	}
 
-}
 
-void goStairs() {
-	while (!pq1.empty() && !pq2.empty()) {
+void steps() {
+	vector<int>st1ing;
+	vector<int>st2ing;
 
-		// 둘 다 차있다
-		// 둘의 순번이 같은애들
-		if (!pq1.empty() && !pq2.empty())
-		{
+	canEnter1 = -21e8;
+	canEnter2 = -21e8;
 
-			if (pq1.top().num == pq2.top().num) {
-				if (pq1.top().s1t < pq2.top().s2t)
-					pq2.pop();
-				else
-					pq1.pop();
-			}
-		}
-
-		if (stair[0].t > stair[1].t)
-		{
-			if (idx1 < 3) {
-				for (int t = 0; t < 3; t++)
-					steps(0);
-				for (int t = 0; t < 3; t++)
-					steps(1);
-			}
-			else
-			{
-				steps(0);
-				steps(1);
-			}
+	// 0번 계단에 선택된 녀석들
+	int finish[10] = { 0, };
+	for (int i = 0; i < path.size(); i++) {
+		finish[path[i]] = 1;
+		if (i < 3) {
+			st1ing.push_back(path[i]);
+			canEnter1 = max(canEnter1,people[path[i]].s1t);
 		}
 		else {
-			if (idx2 < 3) {
-				for (int t = 0; t < 3; t++)
-					steps(1);
-				for (int t = 0; t < 3; t++)
-					steps(0);
+			// 들어갈 수 없는 상황
+			if (people[path[i]].s1t - people[st1ing[i - 3]].s1t < stair[0].t) {
+				canEnter1 = max(canEnter2,people[st1ing[i-3]].s1t + stair[0].t);
+				st1ing.push_back(path[i]);
 			}
+			// 들어갈 수 있다
 			else
 			{
-				steps(1);
-				steps(0);
+				canEnter1 = max(canEnter1,people[path[i]].s1t);
+				st1ing.push_back(path[i]);
+			}
+		}
+	};
+	int st2_cnt = 0;
+
+	for (int i = 0; i < p_cnt; i++) {
+		if (finish[i] == 1)continue;
+		if (st2_cnt < 3) {
+			st2ing.push_back(i);
+			canEnter2 = max(canEnter2,people[i].s2t);
+		}
+		else {
+			// 들어갈 수 없는 상황
+			int check = st2ing[st2_cnt - 3];
+			if (people[i].s2t - people[check].s2t < stair[1].t) {
+				canEnter2 = max(canEnter2, people[check].s2t + stair[1].t);
+				st2ing.push_back(i);
+			}
+			// 들어갈 수 있다
+			else
+			{
+				canEnter2 = max(canEnter2, people[i].s2t);
+				st2ing.push_back(i);
 			}
 		}
 	}
+	cout << "\n 1번 계단 이용자 : ";
+	for (int i = 0; i < st1ing.size(); i++) {
+		cout << st1ing[i] << " ";
+	}cout << "\n 2번 계단 이용자 : ";
+	for (int i = 0; i < st2ing.size(); i++) {
+		cout << st2ing[i] << " ";
+	}cout << '\n';
+	cout << canEnter1 << " " << canEnter2 << '\n';
+
+	int MIN;
+	if (canEnter1 != -21e8 && canEnter2 != -21e8)
+		MIN = max(canEnter1, canEnter2);
+	else if (canEnter1 == -21e8)
+		MIN = canEnter2;
+	else if (canEnter2==-21e8)
+		MIN = canEnter1;
+
+	MINTIME = min(MINTIME, MIN);
+}
+
+void dfs(int level) {
+	if (level == p_cnt)
+	{
+		steps();
+		return;
+	}
+	// 0번 계단으로 갈 애들 고르기
+	path.push_back(level);
+	dfs(level + 1);
+	path.pop_back();
+	// 안고르기
+	dfs(level + 1);
+	
+
 		
 }
 
 int main() {
-	freopen("input.txt", "r", stdin);
+	//freopen("input.txt", "r", stdin);
 	int T;
 	cin >> T;
 	for (int tc = 1; tc <= T; tc++) {
 		reset();
 		input();
 		calctime();
-		goStairs();
-		cout << "#" << tc << " " << max(canEnter1, canEnter2) +1<< '\n';
+		sort(people, people + p_cnt, cmps1);
+		dfs(0);
+		cout << "#" << tc << " " << MINTIME +1 << '\n';
 		
 	}
 }
 
-
-/*
-10
-0 0 0 0 0 0 0 0 0 0
-0 0 0 0 1 0 0 0 0 0
-0 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0
-3 0 1 0 1 0 0 0 0 2
-1 1 0 0 1 0 1 0 0 0
-0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0
-
-*/
