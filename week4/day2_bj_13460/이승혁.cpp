8a8cbfb35c99cde2de63e6cb0728ea0dd4e6d0d6
leeseunghyuk0228 <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-02-28 09:01:25 +0900

    0228Morning

--- week4/day2_bj_13460/이승혁.cpp
+++ week4/day2_bj_13460/이승혁.cpp
@@ -1,175 +1,151 @@
-############# 설계 간지 ##############
-
-R좌표 -> dfs(sy,sx)
-sy, sx 기준, 4방향 탐색 ->( 1.벽이다 2. 구멍이다. 3.파란공이다. 아니면 갈 수 있다)
-
-갈수있는 방향을 rotate 함수 전달
-(rotate 전 MAP 저장)
-전달받은 방향으로 AfterRotate.y, AfterRotate.x 생성
-
-A.y, A.x O인지 확인 -> 아니라면 dfs(A.y, A.x)
-
-#######################################
-=> 파란색도 움직이나?
-=> 상으로 갔으면 그 다음에는 하로 올 수 없게? -> dfs(x,y,상) -> dfs(x,y,하) -> dfs(x,y,상) .....                             ######
-  => 갔다가 막히면 돌아오는 방법.. Rotate 했는데 자기 자신이라면 return,(우 뱡향으로 왔는데, 내오른쪽/위/아래 벽이면 return) ->     R #
-                                                                                                                          ######
-while 을 사용해서 돌리는게 나은가? => 갈 수 있는 곳을 찾아도 갔다가 돌아와야하지 않을까 안돌아와도 된다는 보장은?
-
-
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-struct coord {
-	int y, x;
-};
-string MAP[10];
-int N, M, ty, tx, sy, sx, ans = 0, cnt = 0, recent = 4, check[] = { 1,0,3,2,-1 };
-
+int  N,M,cnt, ans,ty,tx;
 int ydir[] = { -1,1,0,0 };
 int xdir[] = { 0,0,-1,1 };
+string MAP[10];
+struct coord { int y, x; };
 
+coord RED, BLUE;
+
+// 입력 함수
 void input() {
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
 		cin >> MAP[i];
 		for (int j = 0; j < M; j++) {
 			if (MAP[i][j] == 'R') {
-				sy = i;
-				sx = j;
+				RED.y = i;
+				RED.x = j;
 			}
-			if (MAP[i][j] == '0') {
+			if (MAP[i][j] == 'B') {
+				BLUE.y = i;
+				BLUE.x = j;
+			}
+			if (MAP[i][j] == 'O') {
 				ty = i;
 				tx = j;
 			}
 		}
 	}
-};
-
-coord rotate(int y,int x,int d) {
-	// 위로 기울이기
-	if (d == 0) {
-		// 현재 y좌표로부터 가장 위쪽('#'또는 'O'를 만날대까지 증가시킨다)
-		for (int i = y-1; i > 0; i--) {
-			if (MAP[i][x] == 'O') {
-				ans = cnt;
-				y = i;
-				break;
-			}
-			if (MAP[i][x] == '#'||MAP[i][x]=='B') {
-				y = i + 1;
-				break;
-			}
-			y = i;
-		}
-
-	}
-	// 아래로 기울이기
-	else if (d == 1) {
-		for (int i = y +1; i < N; i++) {
-			if (MAP[i][x] == 'O') {
-				ans = cnt;
-				y = i;
-				break;
-			}
-			if (MAP[i][x] == '#'|| MAP[i][x] == 'B'){
-				y = i - 1;
-				break;
-			}
-			y = i;
-		}
-	}
-	// 왼쪽으로 기울이기
-	else if (d == 2) {
-		for (int i = x-1; i >0; i--) {
-			if (MAP[y][i] == 'O') {
-				ans = cnt;
-				x = i;
-				break;
-			}
-			if (MAP[y][i] == '#'|| MAP[y][i] == 'B') {
-				x = i + 1;
-				break;
-			}
-			x = i;
-		}
-
-	}
-	// 오른쪽으로 기울이기
-	else if (d == 3) {
-		for (int i = x +1; i < M; i++) {
-			if (MAP[y][i] == 'O') {
-				ans = cnt;
-				x = i;
-				break;
-			}
-			if (MAP[y][i] == '#'|| MAP[y][i] == 'B') {
-				x = i -1;
-				break;
-			}
-			x = i;
-		}
-
-	}
-	return { y, x };
 }
 
-void func(int y,int x) {
 
-	if (cnt >= 10) {
-		if (ans == 0) 
-			ans = -1;
+// 구슬 회전 함수
+void rotate(int direction) {
+	int nry = RED.y;
+	int nrx = RED.x;
+	int nby = BLUE.y;
+	int nbx = BLUE.x;
+
+	while (1) {
+		// 구슬 direction 방향으로 이동시켰다
+		// 빨간 구슬과 파란 구슬 모두 이동 가능하다면
+		nry = RED.y+ ydir[direction];
+		nrx = RED.x +xdir[direction];
+		nby = BLUE.y +ydir[direction];
+		nbx = BLUE.x +xdir[direction];
+
+		// 빨간 구슬만 이동 가능하다면
+		if ((MAP[nby][nbx] == '#' || MAP[nby][nbx] == 'R') && (MAP[nry][nrx] == '.' || MAP[nry][nrx] == 'O'))
+		{
+			RED.y = nry;
+			RED.x = nrx;
+		}
+		// 파란 구슬만 이동 가능하다면
+		if ((MAP[nry][nrx] == '#' || MAP[nry][nrx] == 'B') && (MAP[nby][nbx] == '.' || MAP[nby][nbx] == 'O'))
+		{
+			BLUE.y = nry;
+			BLUE.x = nbx;
+		}
+
+		// 둘 다 이동할 수 없다면
+		if (MAP[nry][nrx] == '#' && MAP[nby][nbx] == '#')break;
+	};
+
+	// 구슬의 최종 좌표 완성
+	RED.y = nry;
+	RED.x = nrx;
+	BLUE.y = nby;
+	BLUE.x = nbx;
+}
+
+// 맵 변경 함수
+void changeMAP(int nowry,int nowrx,int nowby,int nowbx) {
+	MAP[nowry][nowrx] = '.';
+	MAP[nowby][nowbx] = '.';
+	MAP[RED.y][RED.x] = 'R';
+	MAP[BLUE.y][BLUE.x] = 'B';
+}
+
+// dfs
+void dfs(int d) {
+	// 들어왔을때 기저조건 1 확인
+	// 기저조건 1
+	if (cnt >= 10) return;
+
+	// 회전하기 전 좌표 저장
+	int nowry = RED.y;
+	int nowrx = RED.x;
+	int nowby = BLUE.y;
+	int nowbx = BLUE.x;
+
+	cnt++;
+	rotate(d);
+	// 돌린 후 기저조건 2 확인
+	// 기저조건 2
+	if (MAP[RED.y][RED.x] == 'O' && MAP[BLUE.y][BLUE.x] == 'O' || MAP[RED.y][RED.x] != 'O' && MAP[BLUE.y][BLUE.x] == 'O') {
+		ans = -1;
 		return;
 	}
+	else if (MAP[RED.y][RED.x] == 'O') {
+		ans = cnt;
+		return;
+	}
+	
+
+	// d 방향으로 돌렸는데 더 돌릴 수 있다면 또다시 방향을 확인해본다
+	// 맵 저장하고
+	string backup[10];
+	memcpy(backup, MAP, N * sizeof(string));
+	// 맵 변경
+	changeMAP(nowry, nowrx, nowby, nowbx);
 
 	for (int i = 0; i < 4; i++) {
-		int ny = y + ydir[i];
-		int nx = x + xdir[i];
-
-		if (ny < 0 || nx < 0 || ny >= N || nx >= M)continue;
-		if (i == check[recent])continue;
+		int ny = RED.y + ydir[i];
+		int nx = RED.x + xdir[i];
 		if (MAP[ny][nx] == '#')continue;
 		if (MAP[ny][nx] == 'O') {
 			ans = cnt + 1;
 			return;
 		}
-
-		string backup[10];
-
-		memcpy(backup, MAP, N  * sizeof(string));
-
-		MAP[y][x] = '.';
-		cnt++;
-		recent = i;
-
-		coord afterRotate=rotate(y,x, i);
-		cout << i << "방향 회전해서 " << afterRotate.y << "," << afterRotate.x << "좌표로 이동\n";
-		cout << "현재 회전 수 :" << cnt << '\n';
-
-		if (MAP[afterRotate.y][afterRotate.x] == 'O') return;
-		else if (afterRotate.y == y && afterRotate.x == x)return;
-		else {
-			MAP[afterRotate.y][afterRotate.x] = 'R';
-			for (int i = 0; i < N; i++) {
-				cout << MAP[i] << '\n';
-			}cout << '\n';
-			func(afterRotate.y, afterRotate.x);
-			memcpy(backup, MAP, N * sizeof(string));
-			recent = 0;
-			cnt--;
-		}
-		
+		dfs(d);
+		// 맵 복구
+		memcpy(MAP, backup, N * sizeof(string));
+		// 구슬 좌표 복구
+		RED.y = nowry;
+		RED.x = nowrx;
+		BLUE.y = nowby;
+		BLUE.x = nowbx;
 	}
-
 }
 
+
 int main() {
-	// input()
 	input();
-	// solve()
-	cout << '\n';
-	func(sy, sx);
-	// output()
-	cout << ans;
-	return 0;
+	string backup[10];
+	memcpy(backup, MAP, N * sizeof(string));
+	// 빨간공 기준 이동 가능 방향 확인하기
+	for (int i = 0; i < 4; i++) {
+		int y = RED.y + ydir[i];
+		int x = RED.x + xdir[i];
+		if (MAP[y][x] != '.')continue;
+		cnt = 0;
+		dfs(i);
+		// dfs 끝나면 맵 복구
+		memcpy(MAP, backup, N * sizeof(string));
+	}
+
 }
