e7ac355ebc363761f983973548e962303c4e035f
seyi103 <74170226+seyi103@users.noreply.github.com>
2023-03-18 19:43:51 +0900

    Create 1748.cpp

---
+++ week6(mar3)/boj/basic/sy/1748.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+int N;
+
+int main() {
+	int res = 0;
+	cin >> N;
+	for (int i = 1; i <= N; i *= 10) {
+		res += N - i + 1;
+	}
+	cout << res;
+	return 0;
+}
