53981fd9d4d14f8355e7ea12d414f4c8205ce4c9
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2023-04-24 22:56:28 +0900

    Update 강인영.cpp
    해냈다

--- april/week4/day1/강인영.cpp
+++ april/week4/day1/강인영.cpp
@@ -19,87 +19,87 @@ int MAP[17][17];
 int answer;
 
 struct Pipe {
-   int ly;
-   int lx;
-   int ry;
-   int rx;
-   int d; 
+    int ly;
+    int lx;
+    int ry;
+    int rx;
+    int d;
 };
                         //가로    //세로   //대각선
-int dirChance[3][3] = {{1,0,1}, {0,1,1}, {1,1,1}};
-int dirList[3][3] = {{1,0,0}, {0,1,0}, {1,1,1}};
+int dirChance[3][3] = { {1,0,1}, {0,1,1}, {1,1,1} }; //확인해야 할 방향들
+int dirList[3][3] = { {1,0,0}, {0,1,0}, {1,1,1} }; //dir에서 가는향방향
 int dir[3][2] = {
    {0,1}, //가로
    {1,0}, //세로
    {1,1}  //대각선
 };
-vector<int>path;
+vector<int>path; //경로 기담기
 
-void dfs(Pipe nowpipe, vector<int> path) {
+void dfs(Pipe nowpipe) {
 
-   if (nowpipe.ry == N && nowpipe.rx == N) {
-      for (int i = 0; i < path.size(); i++) {
-         cout << path[i] << ' ';
-      }
-      cout << endl;
-      return;
-   }
+    if (nowpipe.ry == N-1 && nowpipe.rx == N-1) {
+        /*for (int i = 0; i < path.size(); i++) {
+            cout << path[i] << ' ';
+        }
+        cout << endl;*/
+        answer++;
+        return;
+    }
 
-   for (int i = 0; i < 3; i++) {
-      int chance = dirChance[nowpipe.d][i];
-      if (chance != 1)
-         continue;
-
-      if (i == 2)
-         int de = 1;
-
-      int flag = 0;
-      int sum = 0;
-      for (int j = 0; j < 3; j++) {
-         if (dirList[i][j] != 1)
-            continue;
-         sum++;
-         int ny = nowpipe.ry + dir[i][0];
-         int nx = nowpipe.rx + dir[i][1];
-
-         if (ny < 0 || nx < 0 || ny >= N || nx >= N)
-            continue;
-         if (MAP[ny][nx] == 1)
+    for (int i = 0; i < 3; i++) {
+        int chance = dirChance[nowpipe.d][i];
+        if (chance != 1)
             continue;
 
-         flag++;
-      }
+        if (i == 2)
+            int de = 1;
 
-      if (flag == sum) {
-         //갈 수 있는 경로
-         int nry = nowpipe.ry + dir[i][0];
-         int nrx = nowpipe.rx + dir[i][1];
+        // 확인해야 할 곳들(sum)이 괜찮으면 flag 값 증가
+        int flag = 0;
+        int sum = 0;
+        for (int j = 0; j < 3; j++) {
+            if (dirList[i][j] != 1)
+                continue;
+            sum++;
+            int ny = nowpipe.ry + dir[j][0];
+            int nx = nowpipe.rx + dir[j][1];
+
+            if (ny < 0 || nx < 0 || ny >= N || nx >= N)
+                continue;
+            if (MAP[ny][nx] == 1)
+                continue;
+
+            flag++;
+        }
         
-         //아니 dfs 너 싫다, 진짜,,
-         dfs(nowpipe, path);
-         path.push_back(i);
-         dfs({ nowpipe.ry, nowpipe.rx, nry, nrx, i }, path);
-         path.pop_back();
-      }
-      
-   }
+        //모두 괜찮아서 해당 방향은 갈 수 있다고 확정됨
+        if (flag == sum) {
+            //갈 수 있는 경로
+            int nry = nowpipe.ry + dir[i][0];
+            int nrx = nowpipe.rx + dir[i][1];
 
+            path.push_back(i);
+            dfs({ nowpipe.ry, nowpipe.rx, nry, nrx, i });
+            path.pop_back();
+        }
+    }
 }
 
 int main() {
 
-   cin >> N;
+    cin >> N;
 
-   for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-         cin >> MAP[i][j];
-      }
-   }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> MAP[i][j];
+        }
+    }
 
-   Pipe start = { 0,0,0,1,0 };
+    // 출발지
+    Pipe start = { 0,0,0,1,0 };
 
-   dfs(start, path);
+    dfs(start);
 
-   cout << answer;
+    cout << answer;
 
 }
